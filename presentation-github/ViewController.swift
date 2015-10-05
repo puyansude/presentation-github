82ca2a8da39e0e57f356fb8822ab9e6d205f91e6
Alain <alain@MacBook-Pro.local>
2015-10-05 11:13:20 -0400

    Initial Commit

---
+++ presentation-github/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  presentation-github
+//
+//  Created by Alain on 15-10-05.
+//  Copyright (c) 2015 Production sur support. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
